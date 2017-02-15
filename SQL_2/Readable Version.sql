26deedca3972a97167e13128242a041d5ccd62bb
fzck <flzauleck@up.edu.ph>
2017-02-15 13:14:16 +0800

    Update Readable Version.sql

--- SQL_2/Readable Version.sql
+++ SQL_2/Readable Version.sql
@@ -1,4 +1,34 @@
+-- COMMANDS
+-- Tables
+-- columns
+
 SELECT title
 FROM Movie
 WHERE director = "Steven Spielberg"
-
+--
+SELECT DISTINCT year 
+FROM Movie
+JOIN Rating
+ON Movie.mID = Rating.mID
+WHERE stars >= 4
+ORDER BY year ASC
+--
+SELECT title
+FROM Movie
+WHERE mID NOT IN (SELECT mID 
+                  FROM Rating)
+--
+SELECT name
+FROM Reviewer
+JOIN Rating
+ON Reviewer.rID = Rating.rID
+WHERE ratingDate IS null
+--
+SELECT name, title, stars, ratingDate
+FROM Reviewer
+JOIN Rating
+ON Reviewer.rID = Rating.rID
+JOIN Movie
+ON Movie.mID = Rating.mID
+ORDER BY name, title, stars
+--
